a27ab5cc6e45e4db2da7ea3ebd9880f5447df055
Irvin Owens Jr <0x8badbeef@sigsegv.us>
2015-03-30 16:27:46 -0700

    Moving to single process method

--- lib/Apoapsis.rb
+++ lib/Apoapsis.rb
@@ -5,6 +5,7 @@ require 'apoapsis/distributable'
 require 'apoapsis/system'
 require 'fileutils'
 require 'singleton'
+require 'fiber'
 
 module Apoapsis
   include Distributable
@@ -13,7 +14,7 @@ module Apoapsis
   include Singleton
   include Network
   # The workers
-  attr_accessor :wolfpack
+  attr_accessor :wolfpack, :work_thread
   # Log setup
   FileUtils.mkpath '/var/log/apoapsis/' unless File.exists?('/var/log/apoapsis')
   FileUtils.touch('/var/log/apoapsis/apoapsis.log') unless File.exists?('/var/log/apoapsis/apoapsis.log')
@@ -39,18 +40,33 @@ module Apoapsis
     # 1. Start processes
     Apoapsis.log.info 'Listeners started.'
     Apoapsis.log.info 'Starting workers...'
-    System.get_processors.each do
-      @wolfpack << fork{
-        Signal.trap('HUP') {
-          Apoapsis.log.info "Process #{Process.pid} exiting..."; exit }
+    65536.times.each do
+      @wolfpack << Fiber.new{
         Apoapsis.instance.run
+        Fiber.yield
       }
       Apoapsis.log.info "Started worker with process id #{@wolfpack.last}"
     end
     Apoapsis.log.info "#{@wolfpack.length } workers running"
-    # 1. TODO: Start listeners once the basic processing system is done
+    # 2. TODO: Start listeners once the basic processing system is done
     Apoapsis.log.info 'Starting listeners...'
     Network.instance.startup
+    Apoapsis.log.info 'Listeners started...'
+    # 3. Start handling tasks
+    @work_thread=Thread.new{
+      loop do
+        Signal.trap('HUP'){
+          puts 'Quitting work!'
+          exit
+        }
+        @wolfpack.each do |fiber|
+          fiber.resume
+        end
+        sleep(0.2)
+      end
+    }
+    Apoapsis.log.info 'Apoapsis ready...'
+    @work_thread.join
   end
 
   # The processing loop
@@ -58,15 +74,12 @@ module Apoapsis
   # on it.  If it fails, the job is gone
 
   def run
-    loop do
-      Signal.trap('HUP'){ break }
-      keys=State.instance.sorted_keys
-      if keys.length > 0
-        job=State.instance.get keys.last
-        State.instance.remove keys.last
-        job.execute
-      end
-      sleep 0.2
+    keys=State.instance.sorted_keys
+    if keys.length > 0
+      job=State.instance.get keys.last
+      State.instance.remove keys.last
+      job.execute
     end
+    sleep 0.2
   end
 end
